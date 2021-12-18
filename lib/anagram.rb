c2602d72af6612b88193c0d23618632d3381246c
Roger Mendez <roger61087@gmail.com>
2021-12-18 16:56:21 -0500

    commit tree 8626a1dbec040b61f31dededd61d0598f552cf8c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,34 @@
 # Your code goes here!
+
+require 'pry'
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+
+        split_word = @word.chars
+        sorted_letters= split_word.sort
+        
+        
+        matches = array.filter do |word|
+            split_array = word.chars
+            sorted_word= split_array.sort
+            sorted_letters == sorted_word
+
+            
+        end
+        # binding.pry
+        matches
+        
+    end
+    
+end
+
+listen = Anagram.new("listen")
+listen.match(%w[enlists google inlets banana])
